58b80c57e06ff7f5663189fa05dfb02a1b216cd6
mmaranda-cs <mmaranda@camsys.com>
2020-07-02 16:43:58 -0400

    Setting up dwolla seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+DwollaSecurity.create(target: "https://api-sandbox.dwolla.com", key: "G7dXkscD5CCxvfBlLFf0xJKsWGkz2nyy2eOvD9UhSGNRiU4lzU", secret:"x5RQzLHnKDKpF3qOonvElYka2tpJwhF1rpTrOhxvOzgtNqjXys")
