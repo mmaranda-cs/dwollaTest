5fd4adc042b39c7c1dbabb481638428c969351a9
mmaranda-cs <mmaranda@camsys.com>
2020-07-09 23:11:31 -0400

    Creating new accounts and  transfers (soon)

--- app/controllers/dwolla_securities_controller.rb
+++ app/controllers/dwolla_securities_controller.rb
@@ -16,35 +16,50 @@ class DwollaSecuritiesController < ApplicationController
     end
 
     # create an application token
-    app_token = $dwolla.auths.client
+    @app_token = $dwolla.auths.client
 
     cusomter_randomizer = rand(100000000000)
 
-    # request_body = {
-    #     :firstName => "Jane#{cusomter_randomizer}",
-    #     :lastName => "Merchant#{cusomter_randomizer}",
-    #     :email => "jmerchant#{cusomter_randomizer}@nomail.net",
-    #     :type => 'receive-only',
-    #     # Fo more information about customer types: https://developers.dwolla.com/concepts/customer-types#customer-types
-    #     # Not required but suggested
-    #     # :ipAddress => '0.0.0.0'
-    #     # businessName => 'TEST BUSINESS'
-    # }
-    #
-    # customer = app_token.post "customers", request_body
-    # customer.response_headers[:location]
+    request_body = {
+        :firstName => "Jane#{cusomter_randomizer}",
+        :lastName => "Merchant#{cusomter_randomizer}",
+        :email => "jmerchant#{cusomter_randomizer}@nomail.net",
+        :type => 'receive-only',
+        # Fo more information about customer types: https://developers.dwolla.com/concepts/customer-types#customer-types
+        # Not required but suggested
+        # :ipAddress => '0.0.0.0'
+        # :businessName => 'TEST BUSINESS'
+    }
 
-    customers = app_token.get "customers", limit: 10
-    for customer in customers
-      puts(customer)
-    end
+    customer = @app_token.post "customers", request_body
+    customer.response_headers[:location]
+
+    customers = @app_token.get "customers", limit: 30
+    @customer_overview = customers._embedded['customers']
+
 
   end
 
   def show
-    @dwolla_endpoint = DwollaSecurity.find(params[:id])
+    customers = @app_token.get "customers", limit: 30
   end
 
+  # ADD THE FOLLOWING METHOD/ACTIONS BELOW
+
+  def new
+    sec = DwollaSecurity.new
+  end
+
+  def create
+    redirect_to dwolla_security_path(@robot)
+  end
+
+  # ADD IN A PRIVATE METHOD FOR ROBOT_PARAMS
+
+  private
+  def robot_params
+    params.permit(:merchant_id, :merchant_first_name, :merchant_last_name, :merchant_email, :account_routing_number, :account_number, :bank_account_type, :name_for_account, :transfer_ammount, :transfer_source)
+  end
 
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+DwollaSecurity.create(target: "https://api-sandbox.dwolla.com", key: "G7dXkscD5CCxvfBlLFf0xJKsWGkz2nyy2eOvD9UhSGNRiU4lzU", secret:"x5RQzLHnKDKpF3qOonvElYka2tpJwhF1rpTrOhxvOzgtNqjXys")
